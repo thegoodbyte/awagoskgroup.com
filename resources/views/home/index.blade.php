b93bec79f54d9ba3a9d487b40cd263895ff6a2a3
Martin Halla <thegoodbyte@gmail.com>
2024-12-17 06:54:20 -0500

    updated index

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -42,7 +42,7 @@
                                     <mask id="coming_mask_0" x="0" y="0">
                                         <rect class="coming-alpha" x="0" y="0" width="100%" height="100%"></rect>
                                         <text class="count number" x="50%" y="45%" text-anchor="middle"
-                                              alignment-baseline="middle">25</text>
+                                              alignment-baseline="middle">30</text>
                                         <text class="count title" x="50%" y="70%" text-anchor="middle"
                                               alignment-baseline="middle">YEARS OF EXPERIENCE</text>
                                     </mask>
